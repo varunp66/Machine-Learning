f04b8b1dc89e8543d9871793bf65d849da67d271
Varun Pathak <varun_yashu88@yahoo.com>
2017-12-09 14:07:42 -0500

    Add files via upload

--- Neural Network.py
+++ Neural Network.py
@@ -1,90 +1,20 @@
 import numpy as np
-import time
 
-n_hidden = 10
-n_in = 10
-n_out = 10
-n_samples = 300
+input_data = np.array([2,3])     # The input has two values 2 and 3
 
-learning_rate = 0.01
-momentum = 0.9
+weights = {'node_0':np.array([1,1]), 'node_1':np.array([-1,1]), 'output': np.array([2,-1])}
+#We have define the weights of each input node and output
 
-np.random.seed(0)
+node_0_value = (input_data * weights['node_0']).sum()
 
-def sigmoid(x):
-    return 1.0/(1.0 + np.exp(-x))
+node_1_value = (input_data * weights['node_1']).sum()
 
-def tanh_prime(x):
-    return  1 - np.tanh(x)**2
+hidden_layer_values = np.array([node_0_value, node_1_value])
 
-def train(x, t, V, W, bv, bw):
+output = (hidden_layer_values * weights['output']).sum()
 
-    # forward
-    A = np.dot(x, V) + bv
-    Z = np.tanh(A)
+print(hidden_layer_values)
 
-    B = np.dot(Z, W) + bw
-    Y = sigmoid(B)
+print(output)
 
-    # backward
-    Ew = Y - t
-    Ev = tanh_prime(A) * np.dot(W, Ew)
-
-    dW = np.outer(Z, Ew)
-    dV = np.outer(x, Ev)
-
-    loss = -np.mean ( t * np.log(Y) + (1 - t) * np.log(1 - Y) )
-
-    # Note that we use error for each layer as a gradient
-    # for biases
-
-    return  loss, (dV, dW, Ev, Ew)
-
-def predict(x, V, W, bv, bw):
-    A = np.dot(x, V) + bv
-    B = np.dot(np.tanh(A), W) + bw
-    return (sigmoid(B) > 0.5).astype(int)
-
-# Setup initial parameters
-# Note that initialization is cruxial for first-order methods!
-
-V = np.random.normal(scale=0.1, size=(n_in, n_hidden))
-W = np.random.normal(scale=0.1, size=(n_hidden, n_out))
-
-bv = np.zeros(n_hidden)
-bw = np.zeros(n_out)
-
-params = [V,W,bv,bw]
-
-# Generate some data
-
-X = np.random.binomial(1, 0.5, (n_samples, n_in))
-T = X ^ 1
-
-# Train
-for epoch in range(100):
-    err = []
-    upd = [0]*len(params)
-
-    t0 = time.clock()
-    for i in range(X.shape[0]):
-        loss, grad = train(X[i], T[i], *params)
-
-        for j in range(len(params)):
-            params[j] -= upd[j]
-
-        for j in range(len(params)):
-            upd[j] = learning_rate * grad[j] + momentum * upd[j]
-
-        err.append( loss )
-
-    print "Epoch: %d, Loss: %.8f, Time: %.4fs" % (
-                epoch, np.mean( err ), time.clock()-t0 )
-
-# Try to predict something
-
-x = np.random.binomial(1, 0.5, n_in)
-print "XOR prediction:"
-print x
-print predict(x, *params)
 
