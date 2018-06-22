06a3e39f9620f75e8819738c14fadaa42a73e4cb
Varun Pathak <varun_yashu88@yahoo.com>
2018-06-22 00:15:14 -0400

    Add files via upload

---
+++ KNN(Trainig and Prediction).py
@@ -0,0 +1,38 @@
+#############        TRAINING           #######################
+# Import KNeighborsClassifier from sklearn.neighbors
+from sklearn.neighbors import KNeighborsClassifier
+
+# Create arrays for the features and the response variable
+y = df['party'].values
+X = df.drop('party', axis=1).values
+
+# Create a k-NN classifier with 6 neighbors
+knn = KNeighborsClassifier(n_neighbors = 6)
+
+# Fit the classifier to the data
+#Here we have use Fit method to train our model.
+knn.fit(X,y)
+
+
+#################   PREDICTION   #############################
+
+
+# Import KNeighborsClassifier from sklearn.neighbors
+from sklearn.neighbors import KNeighborsClassifier 
+
+# Create arrays for the features and the response variable
+y = df['party'].values
+X = df.drop('party', axis=1).values
+
+# Create a k-NN classifier with 6 neighbors: knn
+knn = KNeighborsClassifier(n_neighbors=6)
+
+# Fit the classifier to the data
+knn.fit(X,y)
+
+# Predict the labels for the training data X
+y_pred = knn.predict(X)
+
+# Predict and print the label for the new data point X_new
+new_prediction = knn.predict(X_new)
+print("Prediction: {}".format(new_prediction))
